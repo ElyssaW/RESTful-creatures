0df381f892a975f97aa44b243db4b333b92a48f1
ElyssaW <ejwinch1@gmail.com>
2021-01-12 20:56:08 -0600

    Added route to display all prehistoric creatures

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const { response } = require('express')
+// Require express
 const express = require('express')
+// Require EJS
 const layouts = require('express-ejs-layouts')
+// Require file read/write
 const fs = require('fs')
 const { parse } = require('path')
 const { stringify } = require('querystring')
 
+// Instatiate Express
 let app = express()
 
+// Set view engine to EJS
 app.set('view engine', 'ejs')
 app.use(layouts)
 
 // Body parser
 app.use(express.urlencoded({extended: false}))
 
+//Show home screen
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+// Show dinosaurs in JSON file
 app.get('/dinosaurs', (req, res) => {
     let dinosaurs = fs.readFileSync('./dinosaurs.json')
     let dinoData = JSON.parse(dinosaurs)
     res.render('dinosaurs/index.ejs', {myDinos: dinoData})
 })
 
+app.get('/prehistoric_creatures', (req, res) => {
+    let prehistoricCreatures = fs.readFileSync('./prehistoric_creatures.json')
+    let creaturesData = JSON.parse(prehistoricCreatures)
+    console.log(creaturesData)
+    res.render('prehistoric_creatures/index.ejs', {myCreatures: creaturesData})
+})
+
+// Show page to write new dinosaur
 app.get('/dinosaurs/new', (req, res) => {
     res.render('dinosaurs/new.ejs')
 })
 
+// Write new dinosaur to file, called on submit event in /dinosaurs/new
 app.post('/dinosaurs', (req, res) => {
     let dinosaurs = fs.readFileSync('./dinosaurs.json')
     let dinoData = JSON.parse(dinosaurs)
@@ -36,6 +52,7 @@ app.post('/dinosaurs', (req, res) => {
     res.redirect('dinosaurs')
 })
 
+// Shows specific dinosaur
 app.get('/dinosaurs/:id', (req, res) => {
     let dinosaurs = fs.readFileSync('./dinosaurs.json')
     let dinoData = JSON.parse(dinosaurs)
